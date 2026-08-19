8345da0ae634254dafbf0b71b3133834482172aa
ashina <tajitajifj0133@gmail.com>
2026-08-20 02:07:53 +0900

    feat(titles): v2基盤の公開入口を追加

---
+++ packages/core/src/titles/v2.ts
@@ -0,0 +1,28 @@
+export {
+  TITLE_SOURCES,
+  TITLE_TIME_ZONE,
+  defineTitle,
+  type TitleCheckResult,
+  type TitleDefinition,
+  type TitleEpochPolicy,
+  type TitleLifecycle,
+  type TitleSourceCodeRef,
+  type TitleSourceDefinition,
+  type TitleSourceKey,
+  type TitleSourceKind,
+  type TitleSourcePrivacy,
+  type TitleTrigger,
+} from "./v2-contract.js";
+
+export {
+  TitleV2Store,
+  ensureTitleV2Schema,
+  type ApplyCatalogInput,
+  type AwardTitleInput,
+  type TitleAwardRow,
+  type TitleBaselineRow,
+  type TitleBaselineRunRow,
+  type TitleCatalogEpochRow,
+  type TitleEquipRow,
+  type TitleSystemStateRow,
+} from "./v2-store.js";
